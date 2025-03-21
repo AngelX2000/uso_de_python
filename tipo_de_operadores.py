a033f99e5de9f2070c4e2960ac8483eeadb0f6ed
AngelX2000 <miguel_osorio11@hotmail.com>
2025-03-20 20:00:28 -0500

    clase 20 de marzo

---
+++ tipo_de_operadores.py
@@ -0,0 +1,41 @@
+'''
+operadores aritmeticos
+20-marzo-2025
+
+'''
+
+#operador suma
+print(2+6)
+ 
+#operador resta
+print(8-4)
+
+'''
+operadores de asignacion
+'''
+#asignacion de igualdad o definicion
+w=5
+
+#incremento
+saldo=100
+saldo=saldo+1
+print(saldo)
+
+#decrecimiento
+
+
+
+
+'''
+operadores logicos
+and: comprueban si todas las condiciones se cumplen, true, flase de lo contrario
+or: comprueba si alguna de las condiciones se cumple , true, false de lo contrario
+not: negar el estado de una variable
+'''
+print(x==4 and y==6)
+print(x==4 or y==8)
+print(x==5 or y==8)
+
+usuario_logeado=True
+click_logout=True
+print(not usuario_logeado)
