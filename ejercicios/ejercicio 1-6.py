3702ff7afa00270288ad058439988d7d7db4a843
AngelX2000 <miguel_osorio11@hotmail.com>
2025-03-21 21:36:21 -0500

    clase 20 marzo v2

---
+++ ejercicios/ejercicio 1-6.py
@@ -0,0 +1,8 @@
+'''
+Teniendo en cuenta el ejercicio anterior calcule el 
+residuo con el símbolo de módulo % y entregue la 
+comprobación con los valores resultantes de dividir 
+dos números entregados por el usuario del 
+programa.
+
+'''
--- tipo_de_operadores.py
+++ tipo_de_operadores.py
@@ -4,11 +4,59 @@ operadores aritmeticos
 
 '''
 
+
+#para realizar comentarios se utilizan los operadores de comillas triples y/o el # (''' y #)
+
 #operador suma
-print(2+6)
- 
+#print(2+6)
+
+#operador multiplicacion
+#print(2*8)
+
 #operador resta
-print(8-4)
+#print(8-4)
+
+#podemos guardar un valor en una variable poniendo el nombre de la variable seguido de el simbolo = y despues el valor
+x=4
+y=6
+#print(x*y)
+
+#operador division
+z=12/4
+#print(z)
+#print(type(z)) #el resultado de la division siempre es flotante
+
+#print(10/3)
+#print(10//3)
+#print(14.5//3)
+
+#la division con doble barra (//) siempre devuelve el entero mas proximo
+
+#print(20/3)
+#print(20%3)
+
+#el "%" se utiliza como modulo, que es el residuo en una operacion
+
+#print(8//-3)
+#print(8/-3)
+
+#para expresar unapotencia utiliza dos veces (**)
+#print(5**2)
+
+'''
+operadores de igualdad (==) y desigualdad (!=)
+
+este tipo de operadores los usamos cuando deseamos comparar
+expresiones o variables. python evalua si se cumple la 
+comparacion y nos devuelve (retorna) un valor True o False
+
+'''
+
+#print(8==8)
+
+#print(3==3.01)
+
+#print(3!=1)
 
 '''
 operadores de asignacion
@@ -16,13 +64,15 @@ operadores de asignacion
 #asignacion de igualdad o definicion
 w=5
 
-#incremento
-saldo=100
-saldo=saldo+1
-print(saldo)
+#incremento: con += podemos incrementar el valor actaul de una variable indicando seguidamente en cuantas unidades queremos afectarla 
+#saldo=100
+#saldo=saldo+1   #esto es igual a la expresion (saldo+=1)
+#print(saldo)
 
-#decrecimiento
-
+#decrecimiento: este operador se utiliza para hacer decrecer el valor actual de una variable, el simbolo utilizado es -= y debe ser acompañado de un valor que ejercerá el efecto de disminucion
+credito=8
+credito=credito-2 #esto es lo mismo que expresar la instruccion (credito-=2)
+print(credito)
 
 
 
@@ -39,3 +89,13 @@ print(x==5 or y==8)
 usuario_logeado=True
 click_logout=True
 print(not usuario_logeado)
+
+
+
+
+
+
+
+
+
+
