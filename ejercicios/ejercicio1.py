ed62f5eb73f5d4f27f7d9f9474712cf12d9ec657
AngelX2000 <miguel_osorio11@hotmail.com>
2025-03-31 22:03:58 -0500

    ejer. hasta el 10 en casa

--- ejercicios/ejercicio1.py
+++ ejercicios/ejercicio1.py
@@ -9,8 +9,8 @@ realice las operaciones de suma, resta, multiplicación, división y módulo.
 numero_1=int(input("escribe un numero entero"))
 numero_2=int(input("escribe un numero entero"))
 
-print(numero_1 + numero_2)
-print(numero_1 - numero_2)
-print(numero_1 * numero_2) 
-print(numero_1 // numero_2)
-print(numero_1 % numero_2)
+print("suma",numero_1 + numero_2)
+print("resta",numero_1 - numero_2)
+print("multiplicacion",numero_1 * numero_2) 
+print("division",numero_1 // numero_2)
+print("modulo",numero_1 % numero_2)
--- ejercicios/ejercicio7.py
+++ ejercicios/ejercicio7.py
@@ -7,3 +7,10 @@ Problema: Almacenar los nombres de 3 ciudades en una tupla y luego mostrar:
 '''
 
 
+tupla_1=( "palmira", "cali", "buga")
+
+print((tupla_1[0]), (tupla_1[2]))
+
+print(len("palmira"),(tupla_1[0]))
+print(len("cali"),(tupla_1[1]))
+print(len("buga"),(tupla_1[2]))
